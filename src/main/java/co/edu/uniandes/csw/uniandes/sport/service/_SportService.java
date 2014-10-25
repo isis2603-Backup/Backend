6960d02dc03dc5f7cfd638360cd1aa4b08d0ffb1
Jhonatan <jhonatan.alarcon.ariza@gmail.com>
2014-10-25 11:17:13 -0500

    Rev Se introduce filter en el web.xml para manejo de Options

--- src/main/java/co/edu/uniandes/csw/uniandes/sport/service/_SportService.java
+++ src/main/java/co/edu/uniandes/csw/uniandes/sport/service/_SportService.java
@@ -89,14 +89,14 @@ public static String URL_SERVICIO = System.getenv("URL1");
          
         
         
-//        @OPTIONS
-//        public Response cors(@javax.ws.rs.core.Context HttpHeaders requestHeaders) {
-//		System.out.println("ENTRO A HEADER");
-//		for(String header : requestHeaders.getRequestHeaders().keySet()){
-//			System.out.println(header);
-//		}
-//            return Response.status(200).header("Access-Control-Allow-Origin", "*").header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS").header("Access-Control-Allow-Headers", "AUTHORIZATION, content-type, accept, X_REST_USER").build();
-//        }
+        @OPTIONS
+        public Response cors(@javax.ws.rs.core.Context HttpHeaders requestHeaders) {
+		System.out.println("ENTRO A HEADER");
+		for(String header : requestHeaders.getRequestHeaders().keySet()){
+			System.out.println(header);
+		}
+            return Response.status(200).header("Access-Control-Allow-Origin", "*").header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS").header("Access-Control-Allow-Headers", "AUTHORIZATION, content-type, accept, X_REST_USER").build();
+        }
 
         
         
